66faa87b54bb45822b3a82dbc120ff82007dcf3c
Koushik K Das <kkdtitu@yahoo.com>
2020-10-24 00:48:01 -0400

    commit Oct 23, 2020

--- sample-pyobdc.py
+++ sample-pyobdc.py
@@ -1,6 +1,7 @@
 import pyodbc
 
 # https://docs.microsoft.com/en-us/sql/connect/python/pyodbc/step-3-proof-of-concept-connecting-to-sql-using-pyodbc?view=sql-server-ver15
+# look at odbc-sqlserver-install-pyodbc.txt
 
 # Some other example server values are
 # server = 'localhost\sqlexpress' # for a named instance
