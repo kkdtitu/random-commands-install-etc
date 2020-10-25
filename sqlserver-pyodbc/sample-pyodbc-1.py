22879becca1c1cb3b16fb6610995a053543b7350
Koushik K Das <kkdtitu@yahoo.com>
2020-10-25 10:05:34 -0400

    commit Oct 25, 2020

--- sqlserver-pyodbc/sample-pyodbc-1.py
+++ sqlserver-pyodbc/sample-pyodbc-1.py
@@ -8,16 +8,11 @@ import pyodbc
 # server = 'myserver,port' # to specify an alternate port
 
 #Sample 4 values for server, database, username, password are:
-server = 'tcp:myserver.database.windows.net' 
+server = 'tcp:myserver.database.windows.net,1433' 
 database = 'mydb' 
 username = 'myusername' 
 password = 'mypassword' 
 
-#Actual 4 values for server, database, username, password are:
-# 'tcp:isye-6420-project.database.windows.net,1433' 
-# 'topic_modelling' 
-# 'project_administrator' 
-# 'isye_6420_admin'
  
 conn = pyodbc.connect('DRIVER={ODBC Driver 17 for SQL Server};SERVER='+server+';DATABASE='+database+';UID='+username+';PWD='+ password)
 cursor = conn.cursor()
